a208c4de3da4b7a8684ed8d1b917ddf0b067879e
阿尔贝鲁 <cbdyzj@gmail.com>
2025-03-13 20:30:21 +0800

    Initial commit

---
+++ src/components/AppWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <NMessageProvider>
+    <App />
+  </NMessageProvider>
+</template>
+
+<script setup lang="ts">
+import { NMessageProvider } from 'naive-ui'
+import App from '@/components/App.vue'
+</script>
+
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
