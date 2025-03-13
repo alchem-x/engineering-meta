9f98400136f529a973fd4f7efab0eaafa893bfda
阿尔贝鲁 <cbdyzj@gmail.com>
2025-03-13 20:30:21 +0800

    Initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+    <b>engineering-meta: {{ getHello() }}</b>
+  </div>
+</template>
+
+<script setup>
+import { getHello } from '@/utils/getHello.ts'
+</script>
+
+<style scoped lang="less">
+.app-container {
+  padding: 1rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
+
+<style lang="less">
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
